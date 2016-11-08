418fb64e5c963e7c891572be2d2d8c3b6a4a9547
mbornstein <marvin.bornstein@student.hpi.uni-potsdam.de>
2016-11-08 11:41:21 +0100

    add much logic

--- sample_logic/main.py
+++ sample_logic/main.py
@@ -6,13 +6,18 @@ import requests
 
 from flask import Flask, request
 
+ownEndpoint = 'http://172.16.58.36:3000'
 
-marketplaceEndpoint = 'http://127.0.0.1:8000'
+marketplaceEndpoint = 'http://172.16.56.197:8080'
+producerEndpoint = 'http://172.16.59.226:3000'
 
+def getFromListByKey(dictList, key, value):
+    return [elem for elem in dictList if product[k] == value][0]
 
 class MerchantLogic(object):
     def __init__(self):
-        self.merchantID = self.register()
+        self.merchantID = self.registerToMarketplace()
+        self.registerToProducer()
         self.products = self.getProducts()
         self.offers = []
         
@@ -36,44 +41,53 @@ class MerchantLogic(object):
             time.sleep(self.interval)
 
     def getProducts(self):
-        # TODO: add producer API call
-        return [
-            {
-                "product_id": "CD-1",
-                "name": "Jan and Marv EP 1",
-                "condition": 0
-            },
-            {
-                "product_id": "CD-2",
-                "name": "Jan and Marv EP 2",
-                "condition": 0
-            },
-        ]
+        r = requests.get(producerEndpoint + '/buyers')
+        products = [merchant['products'] for merchant in r.json() if merchant['merchant_id'] == self.merchantID][0]
+        for product in products:
+            product['amount'] = 1
+        return products
 
     def createOffer(self, product):
         return {
             "product_id": product['product_id'],
-            "seller_id": "Jan und Marv",
-            "amount": 1337,
-            "price": 42,
-            "shipping_time": 1,
+            "merchant_id": self.merchantID,
+            "amount": product['amount'],
+            "price": product['price'] + 42,
+            "shipping_time": {
+                "standard": 5,
+                "prime": 1
+            },
             "prime": True
         }
 
     def addOfferToMarketplace(self, offer):
         r = requests.post(marketplaceEndpoint + '/offers', json=offer)
-        print("new offer, ID:", r.text)
-        return int(r.text)
+        return r.json()['offer_id']
 
-    def register(self):
-        return 0
+    def registerToMarketplace(self):
+        requestObject = {
+            "api_endpoint_url": ownEndpoint,
+            "merchant_name": "Sample Merchant",
+            "algorithm_name": "IncreasePrice"
+        }
+        r = requests.post(marketplaceEndpoint + '/merchants', json=requestObject)
+        return r.json()[merchant_id]
+
+    def registerToProducer(self):
+        requestObject = {
+            "merchant_id": self.merchantID
+        }
+        r = requests.post(producerEndpoint + '/buyers')
 
     def adjustPrices(self):
-        offer = self.offers[0]
-        offer['price'] += 1
-        print('update offer price:', offer['price'])
+        offer = random.choice(self.offers)
+        offer['price'] = max(offer['price'] - 1, 5)
+        self.updateOffer(offer)
+
+    def updateOffer(self, newOffer):
+        print('update offer:', newOffer)
         try:
-            r = requests.put(marketplaceEndpoint + '/offers', json=offer)
+            r = requests.put(marketplaceEndpoint + '/offers/{:s}'.format(newOffer['id']), json=newOffer)
         except:
             print('failed to update offer')
 
@@ -85,13 +99,42 @@ class MerchantLogic(object):
         self.getOffers()
         self.adjustPrices()
 
+    def soldProduct(self, offer_id, amount):
+        print('sold {:d} items of the offer {:d}'.format(amount, int(offer_id)))
+        offer = [offer for offer in self.offers if offer['id'] == offer_id][0]
+        offer['amount'] -= 1
+        product = [product for product in self.products if product['product_id'] == offer['product_id']][0]
+        product['amount'] -= 1
+        if (product['amount'] <= 0):
+            print('product {:d} is out of stock!'.format(int(product['product_id'])))
+
+        # sample logic: TODO: improve
+        newProduct = self.buyRandomProduct()
+        offer = getFromListByKey(self.offers, 'product_id', newProduct['product_id'])
+        offer['amount'] = newProduct['amount']
+        offer['price'] += 5
+        self.updateOffer(offer)
+
+    # returns product
+    def buyRandomProduct(self):
+        r = requests.get(producerEndpoint + '/products/buy?merchantID={:s}'.format(self.merchantID))
+        productObject = r.json()
+        print('bought new product', productObject)
+        product = getFromListByKey(self.products, 'product_id', productObject['product_id'])
+        product['amount'] += 1
+        return product
+
 
 app = Flask(__name__)
 
+merchantLogic = None
+
 @app.route('/sold', methods=['POST'])
 def item_sold():
+    global merchantLogic
     offer_id = request.json['offer_id']
     amount = request.json['amount']
+    merchantLogic.soldProduct(offer_id, amount)
     return "ok"
 
 if __name__ == "__main__":

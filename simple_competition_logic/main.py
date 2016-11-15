ac5e58aa4f045e6811abb5d9254b142c1b7576c2
mbornstein <marvin.bornstein@student.hpi.uni-potsdam.de>
2016-11-15 17:52:08 +0100

    make it run

--- simple_competition_logic/main.py
+++ simple_competition_logic/main.py
@@ -142,6 +142,13 @@ class MerchantLogic(object):
         print('unRegisterToProducer')
         r = requests.delete(settings['producerEndpoint'] + '/buyers/{:d}'.format(self.merchantID))
 
+    def updateOffer(self, newOffer):
+        print('update offer:', newOffer)
+        try:
+            r = requests.put(settings['marketplaceEndpoint'] + '/offers/{:d}'.format(newOffer['id']), json=newOffer)
+        except Exception as e:
+            print('failed to update offer', e)
+
     def adjustPrices(self, offer, minPrice):
         if minPrice < 16: 
             offer['price'] = 32
@@ -188,7 +195,7 @@ class MerchantLogic(object):
         offer = getFromListByKey(self.offers, 'product_id', newProduct['product_id'])
         print('in this offer:', offer)
         offer['amount'] = newProduct['amount']
-        r = requests.patch(marketplaceEndpoint + '/offers/'+offer['id']+'/restock', json={'amount': 1})
+        r = requests.patch(settings['marketplaceEndpoint'] + '/offers/{:d}/restock'.format(offer['id']), json={'amount': 1})
 
     # returns product
     def buyRandomProduct(self):
@@ -249,18 +256,22 @@ def set_state():
 @app.route('/sold', methods=['POST'])
 def item_sold():
     global merchantLogic
-    
-    if merchantLogic:
-        print(request.json)
-        # ignore 'consumer_id' and 'prime'
-        offer_id = request.json['offer_id']
-        amount = request.json['amount']
-        price = request.json['price']
 
-        consumer_id = request.json['consumer_id'] if 'consumer_id' in request.json else ''
-        prime = request.json['prime'] if 'prime' in request.json else ''
+    if merchantLogic:
+        sentJSON = request.get_json(force=True)
+        # ignore 'consumer_id' and 'prime'
+        # offer_id = int(sentJSON['offer_id'])
+        # amount = int(sentJSON['amount'])
+        # price = float(sentJSON['price'])
+
+        offer_id = sentJSON['offer_id']
+        amount = sentJSON['amount']
+        price = sentJSON['price']
+
+        # consumer_id = sentJSON['consumer_id'] if 'consumer_id' in sentJSON else ''
+        # prime = sentJSON['prime'] if 'prime' in sentJSON else ''
         
-        print('sold {:d} items of the offer {:d} to {:s} for {:d}'.format(amount, offer_id, consumer_id, price))
+        print('sold {:d} items of the offer {:d} for {:f}'.format(amount, offer_id, price))
         merchantLogic.execQueue.append( (merchantLogic.soldProduct, (offer_id, amount, price)) )
     else:
         print('merchantlogic not started')

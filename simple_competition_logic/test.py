a41f15af5e0d248ba9763a148e4167a0bc91d464
jnslk <jan.selke@student.hpi.uni-potsdam.de>
2016-11-30 12:09:32 +0100

    refactored testcode to reduce dependency on default settings object in merchant implementation

--- simple_competition_logic/test.py
+++ simple_competition_logic/test.py
@@ -8,6 +8,8 @@ class MerchantTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = MerchantApp.app.test_client()
+        rv = self.app.get('/settings')
+        self.settings = dict(json.loads(rv.data.decode("utf-8")))
     
     @staticmethod
     def equal_dicts(self, d1, d2):
@@ -15,59 +17,21 @@ class MerchantTestCase(unittest.TestCase):
     
     def test_get_settings(self):
         rv = self.app.get('/settings')
-        settings = {
-            'merchant_id': 0,
-            'merchant_url': 'http://vm-mpws2016hp1-06.eaalab.hpi.uni-potsdam.de',
-            'marketplace_url': 'http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de',
-            'producerEndpoint': 'http://vm-mpws2016hp1-03.eaalab.hpi.uni-potsdam.de',
-            'priceDecrease': 1,
-            'initialProducts': 5,
-            'minPriceMargin': 16,
-            'maxPriceMargin': 32,
-            'shipping': 5,
-            'primeShipping': 1,
-            'debug': False
-        }
-        response_json = json.loads(rv.data.decode("utf-8"))
-        js = json.dumps(settings)
-        assert self.equal_dicts(self, settings, response_json) & (js in rv.data.decode("utf-8"))
+        response = dict(json.loads(rv.data.decode("utf-8")))
+        assert (self.settings == response) & self.equal_dicts(self, self.settings, response)
         
     def test_post_settings(self):
-        settings = {
-            'merchant_id': 0,
-            'merchant_url': 'http://vm-mpws2016hp1-06.eaalab.hpi.uni-potsdam.de',
-            'marketplace_url': 'http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de',
-            'producerEndpoint': 'http://vm-mpws2016hp1-03.eaalab.hpi.uni-potsdam.de',
-            'priceDecrease': 1,
-            'initialProducts': 5,
-            'minPriceMargin': 17,
-            'maxPriceMargin': 32,
-            'shipping': 5,
-            'primeShipping': 1,
-            'debug': False
-        }
-        rv = self.app.post('/settings',data=json.dumps(dict(settings)),content_type='application/json')
-        response_json = json.loads(rv.data.decode('utf-8'))
-        assert (response_json['minPriceMargin'] == 17) & self.equal_dicts(self, settings, response_json)
+        self.settings['merchant_url'] = 'foo'
+        rv = self.app.post('/settings',data=json.dumps(dict(self.settings)),content_type='application/json')
+        response = dict(json.loads(rv.data.decode('utf-8')))
+        assert (response['merchant_url'] == 'foo') & self.equal_dicts(self, self.settings, response)
 
     
     def test_put_settings(self):
-        settings = {
-            'merchant_id': 0,
-            'merchant_url': 'http://vm-mpws2016hp1-06.eaalab.hpi.uni-potsdam.de',
-            'marketplace_url': 'http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de',
-            'producerEndpoint': 'http://vm-mpws2016hp1-03.eaalab.hpi.uni-potsdam.de',
-            'priceDecrease': 1,
-            'initialProducts': 5,
-            'minPriceMargin': 16,
-            'maxPriceMargin': 33,
-            'shipping': 5,
-            'primeShipping': 1,
-            'debug': False
-        }
-        rv = self.app.put('/settings',data=json.dumps(dict(settings)),content_type='application/json')
-        response_json = json.loads(rv.data.decode('utf-8'))
-        assert (response_json['maxPriceMargin'] == 33) & self.equal_dicts(self, settings, response_json)
+        self.settings['marketplace_url'] = 'bar'
+        rv = self.app.put('/settings',data=json.dumps(dict(self.settings)),content_type='application/json')
+        response = dict(json.loads(rv.data.decode('utf-8')))
+        assert (response['marketplace_url'] == 'bar') & self.equal_dicts(self, self.settings, response)
 
     def test_post_sold(self):
         sold_offer = {
@@ -81,6 +45,8 @@ class MerchantTestCase(unittest.TestCase):
         response_json = json.loads(rv.data.decode('utf-8'))
         assert response_json == {}
         
+
+    
     
 if __name__ == '__main__':
     unittest.main()

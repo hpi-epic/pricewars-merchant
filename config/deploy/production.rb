c079d40b92da6925bfdf857f4c846da1227b3a1d
MrSerth <MrSerth@users.noreply.github.com>
2017-01-11 14:14:51 +0100

    Update token for merchant 1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ server "vpn.framsteg.de", user: "deployer", roles: %w(www)
 # uncomment in case of travis, capistrano will have its own
 set :ssh_options, keys: ["config/deploy_id_rsa"] if File.exist?("config/deploy_id_rsa")
 set :ssh_options, port: 7047
-set :api_token, "cVQsIA7WIBVzuS9VTpUpASnjJUHvUah668zOI7uaHQZCsjiMZDDjf1TyVkuyLc6R"
+set :api_token, "AbSn3B7xZe1imvJPRbow2w4lcRIrKtJTVlIYvPXW4XFrTYHuA6rC2wxeNGWhApBf"
 set :deploy_to, "/var/www/pricewars-merchant"
 
 # Configuration

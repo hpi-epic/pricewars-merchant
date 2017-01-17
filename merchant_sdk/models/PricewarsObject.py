7f763c2f8441d70d554b358ed821fd2ff38fd3a9
mbornstein <marvin.bornstein@student.hpi.uni-potsdam.de>
2017-01-17 17:13:32 +0100

    make pricewars objects printabke

--- merchant_sdk/models/PricewarsObject.py
+++ merchant_sdk/models/PricewarsObject.py
@@ -1,10 +1,12 @@
+import json
+
 class PricewarsObject:
 
     def to_dict(self):
         return self.__dict__
 
     def __repr__(self):
-        return self.to_dict()
+        return json.dumps(self.to_dict())
 
     @classmethod
     def from_dict(cls, d):

8f827107a2f48e235d18e67350a44fe725874510
mbornstein <marvin.bornstein@student.hpi.uni-potsdam.de>
2017-01-17 18:41:51 +0100

    add soldoffer to sdk

--- merchant_sdk/models/__init__.py
+++ merchant_sdk/models/__init__.py
@@ -1,5 +1,6 @@
 from .Offer import Offer
+from .SoldOffer import SoldOffer
 from .Product import Product
 from .MerchantRegisterResponse import MerchantRegisterResponse
 
-__all__ = [Offer, Product, MerchantRegisterResponse]
+__all__ = [Offer, SoldOffer, Product, MerchantRegisterResponse]

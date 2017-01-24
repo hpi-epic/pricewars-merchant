352ed46a719886694b0839c8eafec6136b6fc590
jaSunny <github@alias.framsteg.org>
2017-01-24 11:06:55 +0100

    adding fix price strategy

--- merchant_sdk/strategies/Strategies.py
+++ merchant_sdk/strategies/Strategies.py
@@ -6,7 +6,17 @@ import pandas as pd
 from random import randint
 from datetime import datetime, timedelta
 
-def be_cheapest(market_situation, product_uid, underprice, purchase_price):
+'''
+    The following methods realizing different pricing strategies which can be used dynamically via settings.
+'''
+
+
+'''
+    be_cheapest: Evaluation the current market situation and setting the price with regards to the cheapest player
+                 in the market and the configured underprice to be the new cheapest player.
+'''
+def be_cheapest(market_situation, product_uid, settings, purchase_price):
+    underprice = settings['underprice']
     offers = []
     cheapest_offer = 999
     [offers.append(offer) for i,offer in enumerate(market_situation) if offer["uid"] == product_uid]
@@ -17,7 +27,12 @@ def be_cheapest(market_situation, product_uid, underprice, purchase_price):
             cheapest_offer = offer["price"]
     return (cheapest_offer - underprice)
 
-def be_second(market_situation, product_uid, underprice, purchase_price):
+'''
+    be_second: Evaluation the current market situation and setting the price with regards to the cheapest player
+            in the market and the configured underprice to be the second cheapest player.
+'''
+def be_second(market_situation, product_uid, settings, purchase_price):
+    underprice = settings['underprice']
     offers = []
     most_expensive_offer = 999
     [offers.append(offer) for i,offer in enumerate(market_situation) if offer["uid"] == product_uid]
@@ -27,3 +42,10 @@ def be_second(market_situation, product_uid, underprice, purchase_price):
         if offer["price"] > most_expensive_offer:
             most_expensive_offer = offer["price"]
     return (most_expensive_offer - underprice)
+
+'''
+    use_fix_price: Simple using a fix price based on purchase price and global margin
+'''
+def use_fix_price(market_situation, product_uid, settings, purchase_price):
+    margin = settings['globalProfitMarginForFixPrice']
+    return (purchase_price + margin)

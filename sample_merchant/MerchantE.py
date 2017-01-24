8eb98f4c35ef6ca14c758f5de749de28ddcbdc20
mbornstein <marvin.bornstein@student.hpi.uni-potsdam.de>
2017-01-24 18:36:29 +0100

    fix Merchant E

--- sample_merchant/MerchantE.py
+++ sample_merchant/MerchantE.py
@@ -6,15 +6,15 @@ from merchant_sdk import MerchantBaseLogic, MerchantServer
 from merchant_sdk.api import PricewarsRequester, MarketplaceApi, ProducerApi
 from merchant_sdk.models import Offer
 
-merchant_token = 'llhGt9Tao3YbN1ANPkI1QkDg3LC88EHt0xAu7dVvh4X45hSTSkVsVhZY3lgBzx60 '
+merchant_token = 'llhGt9Tao3YbN1ANPkI1QkDg3LC88EHt0xAu7dVvh4X45hSTSkVsVhZY3lgBzx60'
 
 settings = {
     'merchant_id': MerchantBaseLogic.calculate_id(merchant_token),
     'marketplace_url': 'http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de:8080/marketplace',
     'producerEndpoint': 'http://vm-mpws2016hp1-03.eaalab.hpi.uni-potsdam.de',
     'debug': True,
-    'fixed_margin': 10,
-    'max_amount_of_offers': 25,
+    'fixed_margin_perc': 20,
+    'max_amount_of_offers': 50,
     'shipping': 5,
     'primeShipping': 1,
     'max_req_per_sec': 10,
@@ -86,7 +86,7 @@ class MerchantSampleLogic(MerchantBaseLogic):
     '''
 
     def price_product(self, product):
-        return self.settings['fixed_margin'] + product.price
+        return (1.0 + self.settings['fixed_margin_perc']) * product.price
 
     def execute_logic(self):
         offers = self.marketplace_api.get_offers()
@@ -103,20 +103,24 @@ class MerchantSampleLogic(MerchantBaseLogic):
                 pass
 
         for product in new_products:
-            if product.uid in own_offers_by_uid:
-                offer = own_offers_by_uid[product.uid]
-                offer.amount += product.amount
-                self.marketplace_api.restock(offer.offer_id, amount=product.amount, signature=product.signature)
+            try:
+                if product.uid in own_offers_by_uid:
+                    offer = own_offers_by_uid[product.uid]
+                    offer.amount += product.amount
+                    offer.signature = product.signature
+                    self.marketplace_api.restock(offer.offer_id, amount=product.amount, signature=product.signature)
 
-                offer.price = self.price_product(product)
-                self.marketplace_api.update_offer(offer)
-            else:
-                offer = Offer.from_product(product)
-                offer.price = self.price_product(product)
-                offer.prime = True
-                offer.shipping_time['standard'] = self.settings['shipping']
-                offer.shipping_time['prime'] = self.settings['primeShipping']
-                self.marketplace_api.add_offer(offer)
+                    offer.price = self.price_product(product)
+                    self.marketplace_api.update_offer(offer)
+                else:
+                    offer = Offer.from_product(product)
+                    offer.price = self.price_product(product)
+                    offer.prime = True
+                    offer.shipping_time['standard'] = self.settings['shipping']
+                    offer.shipping_time['prime'] = self.settings['primeShipping']
+                    self.marketplace_api.add_offer(offer)
+            except Exception as e:
+                print('could not handle product:', product, e)
 
         return 1.0 / settings['max_req_per_sec']
 

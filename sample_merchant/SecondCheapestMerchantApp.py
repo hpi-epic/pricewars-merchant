014f85bc0cf921e1b948beee702bf50f5617c7a4
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2017-01-25 14:58:59 +0100

    Include SecondCheapestMerchantApp with price updates

--- sample_merchant/SecondCheapestMerchantApp.py
+++ sample_merchant/SecondCheapestMerchantApp.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import operator
 
 sys.path.append('../')
 from merchant_sdk import MerchantBaseLogic, MerchantServer
@@ -18,7 +19,7 @@ settings = {
     'merchant_url': 'http://vm-mpws2016hp1-06.eaalab.hpi.uni-potsdam.de',
     'marketplace_url': 'http://vm-mpws2016hp1-04.eaalab.hpi.uni-potsdam.de:8080/marketplace',
     'producerEndpoint': 'http://vm-mpws2016hp1-03.eaalab.hpi.uni-potsdam.de',
-    'listedOffers': 10,
+    'listedOffers': 3,
     'shipping': 5,
     'primeShipping': 1,
     'max_req_per_sec': 10,
@@ -73,16 +74,15 @@ class SecondCheapestMerchantApp(MerchantBaseLogic):
         self.update_api_endpoints()
         return self.settings
 
-    def buy_product_and_post_to_marketplace(self):
+    def buy_product_and_post_to_marketplace(self, all_offers):
         print('buy Product and update')
         new_product = self.producer_api.buy_product(merchant_token=self.merchant_token)
-        existing_offers = self.get_existing_uid_offers_from_marketplace(new_product.uid)
+        existing_offers = self.get_existing_uid_offers_from_marketplace(all_offers, new_product.uid)
         target_price = self.get_second_cheapest_price(existing_offers, new_product.price)
-        existing_offer_id = self.is_product_uid_offered_by_myself(existing_offers)
-        self.post_offer(new_product, target_price, existing_offer_id)
+        existing_offer = self.get_own_offer_for_product_uid(existing_offers, new_product.uid)
+        self.post_offer(new_product, target_price, existing_offer)
 
-    def get_existing_uid_offers_from_marketplace(self, product_uid):
-        all_offers = self.marketplace_api.get_offers()
+    def get_existing_uid_offers_from_marketplace(self, all_offers, product_uid):
         uid_offers = [offer for offer in all_offers if offer.uid == product_uid]
         return uid_offers
 
@@ -98,10 +98,10 @@ class SecondCheapestMerchantApp(MerchantBaseLogic):
 
         return second_cheapest_offer
 
-    def is_product_uid_offered_by_myself(self, offers):
-        return next((offer.offer_id for offer in offers if offer.merchant_id == self.merchant_id), None)
+    def get_own_offer_for_product_uid(self, offers, product_uid):
+        return next((offer for offer in offers if offer.merchant_id == self.merchant_id and offer.uid == product_uid), None)
 
-    def post_offer(self, product, price, existing_offer_id):
+    def post_offer(self, product, price, existing_offer):
         new_offer = Offer.from_product(product)
         new_offer.price = price
         new_offer.shipping_time = {
@@ -109,30 +109,61 @@ class SecondCheapestMerchantApp(MerchantBaseLogic):
             'prime': settings['primeShipping']
         }
         new_offer.prime = True
-        if existing_offer_id is None:
+        if existing_offer is None:
             self.marketplace_api.add_offer(new_offer)
         else:
-            self.marketplace_api.restock(existing_offer_id, product.amount, product.signature)
+            self.marketplace_api.restock(existing_offer.offer_id, product.amount, product.signature)
 
-    def get_amount_of_own_offers(self):
-        all_offers = self.marketplace_api.get_offers()
+    def update_offer(self, own_offer, target_price):
+        own_offer.price = target_price
+        self.marketplace_api.update_offer(own_offer)
+
+    def get_amount_of_own_offers(self, all_offers):
         own_offers = [offer for offer in all_offers if offer.merchant_id == self.merchant_id]
-        return len(own_offers)
+        return sum(offer.amount for offer in own_offers)
 
-    def setup(self, existing_offers=0):
+    def adjust_prices(self, all_offers):
+        print('Update offer')
         try:
-            for i in range(settings['listedOffers']):
-                self.buy_product_and_post_to_marketplace()
+            my_offered_product_uids = [offer.uid for offer in all_offers if offer.merchant_id == self.merchant_id]
+            all_offers_i_offer_as_well = [offer for offer in all_offers if offer.uid in my_offered_product_uids]
+
+            offers_per_traded_product = {}
+            for offer in all_offers_i_offer_as_well:
+                offers_per_traded_product[offer.uid] = offers_per_traded_product.get(offer.uid, 0) + 1
+
+            for product_uid in [uid[0] for uid in sorted(offers_per_traded_product.items(), key=operator.itemgetter(1), reverse=True)]:
+                existing_offers_for_product_id = self.get_existing_uid_offers_from_marketplace(all_offers_i_offer_as_well, product_uid)
+                target_price = self.get_second_cheapest_price(existing_offers_for_product_id, 50.0)
+                existing_offer = self.get_own_offer_for_product_uid(existing_offers_for_product_id, product_uid)
+                self.update_offer(existing_offer, target_price)
         except Exception as e:
-            print('error on setup or increasing offer amount:', e)
+            print('error on adjusting prices:', e)
+
+    def refill_offers(self, all_offers=None):
+        if not all_offers:
+            all_offers = self.marketplace_api.get_offers()
+
+        existing_offers = self.get_amount_of_own_offers(all_offers)
+        try:
+            for i in range(settings['listedOffers'] - existing_offers):
+                self.buy_product_and_post_to_marketplace(all_offers)
+        except Exception as e:
+            print('error on refilling offers:', e)
+
+    def setup(self):
+        all_offers = self.marketplace_api.get_offers()
+        self.refill_offers(all_offers)
 
     def execute_logic(self):
-        amount_of_own_offers = self.get_amount_of_own_offers()
-        self.setup(amount_of_own_offers)
+        all_offers = self.marketplace_api.get_offers()
+        self.adjust_prices(all_offers)
+        self.refill_offers(all_offers)
         return self.interval
 
     def sold_offer(self, offer_json):
-        self.buy_product_and_post_to_marketplace()
+        all_offers = self.marketplace_api.get_offers()
+        self.refill_offers(all_offers)
 
 
 '''

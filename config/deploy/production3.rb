2349cbe98e446a12bb85a5e4767c52729ef2bef0
jaSunny <github@alias.framsteg.org>
2017-01-25 11:12:58 +0100

    adding token for cheapest merchant

--- config/deploy/production3.rb
+++ config/deploy/production3.rb
@@ -4,6 +4,7 @@ server "tunnel.framsteg.de", user: "deployer", roles: %w(www)
 # uncomment in case of travis, capistrano will have its own
 set :ssh_options, keys: ["config/id_rsa"] if File.exist?("config/id_rsa")
 set :ssh_options, port: 7047
+set :api_token, "dNVoPYVq4aiAWyTMkjUhpHDq2h2sjvxNJTDh2iY6IHGoGjB1TfiIwvAydCsN8qkQ"
 set :deploy_to, "/var/www/pricewars-merchant3"
 
 after :deploy, "deploy:activate_cheapest_merchant"

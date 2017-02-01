0ee127b27ff1708e20a412adcad6755fd85a16cb
mbornstein <marvin.bornstein@student.hpi.uni-potsdam.de>
2017-02-01 09:59:11 +0100

    add producer products api to sdk

--- merchant_sdk/api/ProducerApi.py
+++ merchant_sdk/api/ProducerApi.py
@@ -1,3 +1,5 @@
+from typing import List, Type
+
 from .PricewarsBaseApi import PricewarsBaseApi
 from ..models import Product
 
@@ -13,3 +15,28 @@ class ProducerApi(PricewarsBaseApi):
         }
         r = self.request('get', 'buy', params=params)
         return Product.from_dict(r.json())
+
+    def get_products(self):
+        r = self.request('get', 'products')
+        return Product.from_list(r.json())
+
+    def add_products(self, products: List[Type[Product]]):
+        product_dict_list = [p.to_dict() for p in products]
+        self.request('post', 'products', json=product_dict_list)
+
+    def update_products(self, products: List[Type[Product]]):
+        product_dict_list = [p.to_dict() for p in products]
+        self.request('put', 'products', json=product_dict_list)
+
+    def get_product(self, product_uid):
+        r = self.request('get', 'products/{}'.format(product_uid))
+        return Product.from_dict(r.json())
+
+    def add_product(self, product: Type[Product]):
+        self.request('post', 'products', json=product.to_dict())
+
+    def update_product(self, product: Type[Product]):
+        self.request('put', 'products/{}'.format(product.uid), json=product.to_dict())
+
+    def delete_product(self, product_uid):
+        self.request('delete', 'products/{}'.format(product_uid))

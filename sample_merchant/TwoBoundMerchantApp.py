0e0caf3de12da8499d4fbcc2bd9f7d16e25520de
Jan Lindemann <jan.lindemann@student.hpi.de>
2017-02-28 17:05:21 +0100

    correctly calculate price for two bound merchant

--- sample_merchant/TwoBoundMerchantApp.py
+++ sample_merchant/TwoBoundMerchantApp.py
@@ -5,9 +5,10 @@ sys.path.append('../')
 from merchant_sdk import MerchantBaseLogic, MerchantServer
 from merchant_sdk.api import PricewarsRequester, MarketplaceApi, ProducerApi
 from merchant_sdk.models import Offer
+import time
 
-merchant_token = "{{API_TOKEN}}"
-#merchant_token = "rYy8nnr1rWKSoTLwgfytbsdCTstIG5A8c9FQw8RndlGTFbQvHPpsIT5lMy4b4Ejg"
+#merchant_token = "{{API_TOKEN}}"
+merchant_token = 'KUZU8W7LzexaimJDvyZ8JhglEZzEFbUZ544DbCYapWBYJUmFSLNm34YIdXWs92SF'
 
 settings = {
     'merchant_id': MerchantBaseLogic.calculate_id(merchant_token),
@@ -15,12 +16,11 @@ settings = {
     'producer_url': MerchantBaseLogic.get_producer_url(),
     'priceDecrease': 0.05,
     'initialProducts': 3,
-    'minPriceMargin': 16,
-    'maxPriceMargin': 32,
+    'minPriceMargin': 3.0,
+    'maxPriceMargin': 12.0,
     'shipping': 1,
     'primeShipping': 1,
-    'max_req_per_sec': 10.0,
-    'underprice': 0.5
+    'max_req_per_sec': 10.0
 }
 
 
@@ -34,6 +34,7 @@ class MerchantD(MerchantBaseLogic):
             Internal state handling
         '''
         self.execQueue = []
+        self.marketplace_requests = []
 
 
         '''
@@ -52,8 +53,8 @@ class MerchantD(MerchantBaseLogic):
             Setup API
         '''
         PricewarsRequester.add_api_token(self.merchant_token)
-        self.marketplace_api = MarketplaceApi(host=self.settings['marketplace_url'])
-        self.producer_api = ProducerApi(host=self.settings['producer_url'])
+        self.marketplace_api = MarketplaceApi(host=self.settings['marketplace_url'], debug=False)
+        self.producer_api = ProducerApi(host=self.settings['producer_url'], debug=False)
 
         '''
             Start Logic Loop
@@ -89,12 +90,29 @@ class MerchantD(MerchantBaseLogic):
         return self.settings
 
     def sold_offer(self, offer):
+        print("sold offer")
         self.execQueue.append((self.sold_product, [offer]))
+
+    def buy_missing_products(self):
+        for i in range(self.settings["initialProducts"] - sum(offer.amount for offer in self.offers.values())):
+            self.buy_product_and_update_offer()
 
     def setup(self):
         try:
-            for i in range(settings['initialProducts']):
-                self.buy_product_and_update_offer()
+            # get all products for later comparison over all qualities
+            self.product = {}
+            for product in self.producer_api.get_products():
+                self.products[product.uid] = product
+
+            # get all existing offers from marketplace
+            self.offer = {}
+            for offer in self.marketplace_api.get_offers():
+                if offer.merchant_id == self.merchant_id:
+                    self.offers[offer.uid] = offer
+
+            # buy new products if none or not enough exist
+            self.buy_missing_products()
+
         except Exception as e:
             print('error on setup:', e)
 
@@ -105,35 +123,45 @@ class MerchantD(MerchantBaseLogic):
         for method, args in tmp_queue:
             method(*args)
 
-        offers = self.marketplace_api.get_offers()
+        # if initialProducts setting increased after start, get new products
+        self.buy_missing_products()
 
-        missing_offers = self.settings["initialProducts"] - len(self.offers)
-        for missing_offer in range(missing_offers):
-            self.buy_product_and_update_offer()
+        self.update_market_situation()
 
-        for product in self.products.values():
-            competitor_offers = []
-            for offer in offers:
-                if offer.merchant_id != self.merchant_id and offer.product_id == product.product_id:
-                    competitor_offers.append(offer.price)
-            offer = self.offers[product.uid]
-            if len(competitor_offers) > 0:
-                offer = self.offers[product.uid]
-                self.adjust_prices(offer=offer, product=product, lowest_competitor_price=min(competitor_offers))
+        return self.calculate_intervall()
 
-        return 70.0/settings['max_req_per_sec']
+    def base_price_diff(self, offer):
+        product = self.products[offer.uid]
+        return offer.price - product.price
 
-    def adjust_prices(self, offer=None, product=None, lowest_competitor_price=0):
+    def adjust_prices(self, offer=None, lowest_price_diff=0):
+        product = self.products[offer.uid]
         if not offer or not product:
             return
-        min_price = product.price + settings['minPriceMargin']
-        max_price = product.price + settings['maxPriceMargin']
-        price = lowest_competitor_price - settings['priceDecrease']
-        price = min(price, max_price)
-        if price < min_price:
-            price = max_price
-        offer.price = price
-        self.marketplace_api.update_offer(offer)
+        price_diff = min(lowest_price_diff - settings['priceDecrease'], settings['maxPriceMargin'])
+        if price_diff < settings['minPriceMargin']:
+            price_diff = settings['maxPriceMargin']
+        new_product_price = product.price + price_diff
+        if new_product_price != offer.price:
+            offer.price = new_product_price
+            print("update to new price ", new_product_price)
+            self.marketplace_api.update_offer(offer)
+            self.request_done()
+
+    def update_market_situation(self):
+        marketplace_offers = self.marketplace_api.get_offers()
+        for own_offer in self.offers.values():
+            if self.quora_exhausted():
+                break
+            if own_offer.amount > 0:
+                competitor_offers_price_diff = []
+                for marketplace_offer in marketplace_offers:
+                    if marketplace_offer.merchant_id != self.merchant_id and marketplace_offer.product_id == own_offer.product_id:
+                        competitor_offers_price_diff.append(self.base_price_diff(marketplace_offer))
+                if len(competitor_offers_price_diff) > 0:
+                    self.adjust_prices(offer=own_offer, lowest_price_diff=min(competitor_offers_price_diff))
+                else:
+                    self.adjust_prices(offer=own_offer)
 
     def sold_product(self, sold_offer):
         print('soldProduct, offer:', sold_offer)
@@ -155,7 +183,7 @@ class MerchantD(MerchantBaseLogic):
             'prime': settings['primeShipping']
         }
         new_offer.prime = True
-        self.products[new_product.uid] = new_product
+        # self.products[new_product.uid] = new_product
         new_offer.offer_id = self.marketplace_api.add_offer(new_offer).offer_id
         self.offers[new_product.uid] = new_offer
 
@@ -175,10 +203,39 @@ class MerchantD(MerchantBaseLogic):
         print('buy Product and update')
         new_product = self.producer_api.buy_product()
 
-        if new_product.uid in self.products:
+        if new_product.uid in self.offers:
             self.restock_existing_product(new_product)
         else:
             self.add_new_product_to_offers(new_product)
+
+    def request_done(self):
+        self.marketplace_requests.insert(0,time.time())
+
+    def quora_exhausted(self):
+        while len(self.marketplace_requests) > 0:
+            last = self.marketplace_requests.pop()
+            now = time.time()
+            if now - last < 1:
+                self.marketplace_requests.append(last)
+                break
+
+        return len(self.marketplace_requests) >= settings['max_req_per_sec']
+
+    def active_offers_count(self):
+        offer_count = 0
+        for offer in self.offers.values():
+            if offer.amount > 0:
+                offer_count += 1
+        return offer_count
+
+    def calculate_intervall(self):
+        if len(self.marketplace_requests) == 0:
+            return 0
+        else:
+            offer_count = self.active_offers_count()
+            remaining_reqs = settings['max_req_per_sec'] - len(self.marketplace_requests)
+            time_to_next_release = time.time() - self.marketplace_requests[len(self.marketplace_requests) - 1]
+            return (offer_count / (remaining_reqs + 1)) * time_to_next_release
 
 
 merchant_logic = MerchantD()

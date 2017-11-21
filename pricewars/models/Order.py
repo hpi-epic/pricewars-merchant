f00647c4f0375fb5c8cdbace2100eb10805cb6b6
Carsten Walther <carsten.walther@student.hpi.de>
2017-11-21 15:03:02 +0100

    rename order entry from price to billing_amount

--- pricewars/models/Order.py
+++ pricewars/models/Order.py
@@ -6,8 +6,9 @@ from pricewars.models import Product
 
 
 class Order(PricewarsObject):
-    def __init__(self, price: float, stock: int, product: Union[Product, dict], left_in_stock: Optional[int] = None):
-        self.price = price
+    def __init__(self, billing_amount: float, stock: int, product: Union[Product, dict],
+                 left_in_stock: Optional[int] = None):
+        self.billing_amount = billing_amount
         self.stock = stock
         self.left_in_stock = left_in_stock
         if type(product) == dict:
